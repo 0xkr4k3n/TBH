273cfbc1730e1282d6b2c0cdd63f292f4ed763ca
Zendour Younes <y.zendour2@gmail.com>
2025-01-11 22:52:38 +0100

    home ui v1 done

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import {ChallengesComponent} from './challenges/challenges.component';
 import {ProfileComponent} from './profile/profile.component';
 import {ScoreboardComponent} from './scoreboard/scoreboard.component';
+import {HomeComponent} from './home/home.component';
 
 export const routes: Routes = [
+  { path: '',component: HomeComponent },
   { path: 'challenges',component: ChallengesComponent },
   { path: 'profile',component: ProfileComponent },
   { path: 'scoreboard',component: ScoreboardComponent },
