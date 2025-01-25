f5c9140eaac94849d0916565e364de3f69d9b536
Nazih Ouchta <75317893+Yvesei@users.noreply.github.com>
2025-01-25 18:26:23 +0100

    Frontend: added categories interface & service

---
+++ Frontend/src/app/service/categories.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../environments/environment.development';
+import {Observable} from 'rxjs';
+import {ChallengeInterface} from '../interface/Challenge.interface';
+import {HttpClient} from '@angular/common/http';
+import {CategoryInterface} from '../interface/category.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private readonly API_ROUTE = environment.apiBaseUrl+"api/categories";
+
+  constructor(private readonly httpClient : HttpClient) {
+  }
+
+  getAllCategories() : Observable<CategoryInterface[]>{
+    return this.httpClient.get<CategoryInterface[]>(this.API_ROUTE);
+  }
+}
